323f9a8d9e303d7643be7a0027806b3a06c548a5
sangyetashi <sangyetashi2@gmail.com>
2023-01-14 21:25:08 +0530

    Who to follow widget is added

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import TrendWidget from './TrendWidget';
+import WhoToFollow from './WhoToFollow.';
 function Search() {
     return (
         <div className="hidden h-screen w-[380px] flex-col space-y-4 overflow-y-scroll px-4 py-2 lg:flex">
@@ -16,27 +17,70 @@ function Search() {
                 <TrendWidget
                     category="Entertainment"
                     title="#RowanAtkinson"
-                    description="Trending with #MrBeans"
+                    description="#MrBeans"
                 />
                 <TrendWidget
-                    category="Entertainment"
-                    title="#RowanAtkinson"
-                    description="Trending with #MrBeans"
+                    category="Sports"
+                    title="#ManUnited"
+                    description="Manchester Derby"
                 />
                 <TrendWidget
-                    category="Entertainment"
-                    title="#RowanAtkinson"
-                    description="Trending with #MrBeans"
+                    category="Sports"
+                    title="#Goat"
+                    description="Messi"
                 />
                 <TrendWidget
-                    category="Entertainment"
-                    title="#RowanAtkinson"
-                    description="Trending with #MrBeans"
+                    category="News"
+                    title="#JoeBiden"
+                    description="#POTUS"
                 />
+                <div className="cursor-pointer rounded-b-2xl px-5 py-4 text-sm text-twitterPurpleLight hover:bg-twittergreylight">
+                    Show more
+                </div>
             </div>
-            <div className="text-lg font-bold ">Who to follow</div>
+            <div className="rounded-2xl bg-twittergrey ">
+                <div className="px-5 py-2 text-xl font-bold ">
+                    Who to follow
+                </div>
+                <WhoToFollow />
+                <WhoToFollow />
+                <WhoToFollow />
+                <WhoToFollow />
+                <div className="cursor-pointer rounded-b-2xl px-5 py-4 text-sm text-twitterPurpleLight hover:bg-twittergreylight">
+                    Show more
+                </div>
+            </div>
         </div>
     );
 }
 
 export default Search;
+
+function undefined({}) {
+    return (
+        <div className="rounded-2xl bg-twittergrey ">
+            <div className="px-5 py-2 text-xl font-bold ">What's happening</div>
+
+            <TrendWidget
+                category="Entertainment"
+                title="#RowanAtkinson"
+                description="Trending with #MrBeans"
+            />
+            <TrendWidget
+                category="Entertainment"
+                title="#RowanAtkinson"
+                description="Trending with #MrBeans"
+            />
+            <TrendWidget
+                category="Entertainment"
+                title="#RowanAtkinson"
+                description="Trending with #MrBeans"
+            />
+            <TrendWidget
+                category="Entertainment"
+                title="#RowanAtkinson"
+                description="Trending with #MrBeans"
+            />
+        </div>
+    );
+}
