00e95f2033bd5e2e99675507d60773a488be824c
sangyetashi <sangyetashi2@gmail.com>
2023-01-14 21:27:20 +0530

    Ui changes with now trends having link

--- src/pages/TrendWidget.jsx
+++ src/pages/TrendWidget.jsx
@@ -4,9 +4,18 @@ export default function TrendWidget({ title, description, category }) {
     return (
         <div className="flex cursor-pointer items-center justify-between py-3 px-5 last:rounded-b-2xl hover:bg-twittergreylight">
             <div>
-                <div className="text-xs font-thin ">{category}</div>
+                <div className="text-xs font-light text-gray-500 ">
+                    {category}
+                </div>
                 <div className="text-sm font-bold ">{title}</div>
-                <div className="text-xs font-thin">{description}</div>
+                <div className="text-xs font-light  text-gray-500">
+                    Trending With{' '}
+                    <span className="text-twitterPurpleLight hover:underline ">
+                        <a href={`https://twitter.com/search?q=${description}`}>
+                            {description}
+                        </a>
+                    </span>
+                </div>
             </div>
             <div className="btn-primary">
                 <MoreHoriz />
